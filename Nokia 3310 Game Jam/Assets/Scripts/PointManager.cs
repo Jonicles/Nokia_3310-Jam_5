62044717bfc22e29e0c1541e3d09f4d6eee5e5e0
skrusberg <skrusberg@gmail.com>
2023-02-19 13:25:03 +0100

    Added point-system, font assets; Changes to menu!

---
+++ Nokia 3310 Game Jam/Assets/Scripts/PointManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointManager : MonoBehaviour
+{
+    private int points;
+    Text pointText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointText = gameObject.GetComponent<Text>();
+
+        //Startar spelet med 0 poäng och uppdaterar UI elementet så att det alltid stämmer
+        points = 0;
+        pointText.text = "Score: " + points;
+    }
+
+    //Metod som kan kallas utifrån för att lägga till poäng till UI
+    public void AddPoints(int addedPoints)
+    {
+        points += addedPoints;
+        pointText.text = "Score: " + points;
+
+    }
+}
